5897a6fdba21215c8381a5cd983c6a8f871cb158
DefryAnanta <defryperanginangin@gmail.com>
2024-10-23 22:30:39 +0700

    Add files via upload

---
+++ Tugas Praktikum 7 Alpro - 2410631170066.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+struct Zodiak {
+    string nama;
+    int awalBulan;
+    int awalHari;
+    int akhirBulan;
+    int akhirHari;
+};
+
+// Fungsi untuk mengambil input tanggal lahir
+void inputTanggalLahir(int &tgl, int &bln, int &thn) {
+    cout << "\nMasukkan Tanggal Lahir Anda (tanggal-bulan-tahun): ";
+    cin >> tgl;
+    char dash1, dash2; // Untuk membaca pemisah antara tanggal, bulan, tahun
+    cin >> dash1;
+    cin >> bln >> dash2;
+    cin >> thn;
+}
+
+// Fungsi untuk menentukan zodiak berdasarkan tanggal lahir
+void tentukanZodiak(int tgl, int bln) {
+    Zodiak zodiak[] = {
+        {"CAPRICORN", 12, 22, 1, 19},
+        {"AQUARIUS", 1, 20, 2, 18},
+        {"PISCES", 2, 19, 3, 20},
+        {"ARIES", 3, 21, 4, 19},
+        {"TAURUS", 4, 20, 5, 20},
+        {"GEMINI", 5, 21, 6, 20},
+        {"CANCER", 6, 21, 7, 22},
+        {"LEO", 7, 23, 8, 22},
+        {"VIRGO", 8, 23, 9, 22},
+        {"LIBRA", 9, 23, 10, 22},
+        {"SCORPIO", 10, 23, 11, 21},
+        {"SAGITTARIUS", 11, 22, 12, 21}
+    };
+
+    string zodiakAnda = "Tidak Diketahui atau Tidak Terbaca, tolong masukkan sesuai format";  // kondisi default atau awal zodiakAnda
+
+    for (int i = 0; i < 12; i++) {
+        if ((bln == zodiak[i].awalBulan && tgl >= zodiak[i].awalHari) ||
+            (bln == zodiak[i].akhirBulan && tgl <= zodiak[i].akhirHari)) {
+            zodiakAnda = zodiak[i].nama;
+            break;
+        }
+    }
+
+    cout << "Zodiak Anda adalah: " << zodiakAnda << endl;  // kondisi akhir jika loop menemukan zodiak yg sesuai
+}
+
+void identitasProgram() {
+    cout << "Tugas Praktikum 7 Pertemuan 8 Alpro" << endl << endl
+         << "Nama       : Defry Ananta Perangin Angin" << endl
+         << "NPM        : 2410631170066" << endl
+         << "Kelas/Prodi: 1B/Informatika" << endl << endl
+         << "Selamat datang di program Menentukan Zodiak\n";
+}
+
+int main() {
+
+    identitasProgram();
+
+    int tgl, bln, thn;
+    char lagi;
+
+    do {
+        // Memanggil fungsi untuk input tanggal lahir
+        inputTanggalLahir(tgl, bln, thn);
+
+        // Memanggil fungsi untuk menentukan zodiak
+        tentukanZodiak(tgl, bln);
+
+        // Menanyakan apakah pengguna ingin mencoba lagi
+        cout << "\nApakah Anda ingin menentukan zodiak lagi? (y/n): ";
+        cin >> lagi;
+
+    } while (lagi == 'y' || lagi == 'Y');
+
+    cout << "Terima kasih dan sampai jumpa!" << endl;
+    return 0;
+}
